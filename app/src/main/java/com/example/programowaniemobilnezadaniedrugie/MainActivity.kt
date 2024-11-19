637ecba2a8a2f0989cc4b005ad82d163b83a0206
Arkadiusz Zięba-Zdziebko <275796@student.pwr.edu.pl>
2024-11-20 00:40:10 +0100

    aaa

--- app/src/main/java/com/example/programowaniemobilnezadaniedrugie/MainActivity.kt
+++ app/src/main/java/com/example/programowaniemobilnezadaniedrugie/MainActivity.kt
@@ -19,10 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProgramowanieMobilneZadanieDrugieTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
                     )
                 }
             }
@@ -30,18 +27,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProgramowanieMobilneZadanieDrugieTheme {
-        Greeting("Android")
-    }
-}
